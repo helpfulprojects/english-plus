0c5a44219f1f65818451705972928862a7e1fea9
Helpful Projects <helpfulprojectsgithub@gmail.com>
2024-06-19 14:33:21 +0300

    Pick text file with code from tutorial. Saved to context for future use.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,52 @@
 import * as vscode from 'vscode';
 
+async function pickFile(): Promise<vscode.TextDocument>{
+    const pickedFiles = await vscode.window.showOpenDialog({
+        filters: {
+          "All files (*.*)": ["*"],
+        },
+        canSelectFolders: false,
+        canSelectFiles: true,
+        canSelectMany: false,
+        openLabel: "Select file with code from tutorial...",
+      });
+  
+    if (!pickedFiles || pickedFiles.length < 1) {
+        throw new Error('Parameter is not a number!');
+    }
+    const pickedFile = pickedFiles[0];
+    const textDocument = await vscode.workspace.openTextDocument(pickedFile);
+    return textDocument
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('english-plus.helloWorld', () => {
-		
+	context.subscriptions.push(vscode.commands.registerCommand('english-plus.openCodeFromTutorial', async (ignoreContextUriPath: boolean = false) => {
+        const editor = vscode.window.activeTextEditor;
+        if(!editor){
+            return
+        }
+        const contextUriPath = context.globalState.get(editor.document.fileName)
+        let tutorialCodeDocument: vscode.TextDocument;
+        if(!contextUriPath || ignoreContextUriPath){
+            tutorialCodeDocument = await pickFile();
+            if(!tutorialCodeDocument){
+                return
+            }
+            context.globalState.update(editor.document.fileName,tutorialCodeDocument.uri.path)
+        }else{
+            tutorialCodeDocument = await vscode.workspace.openTextDocument(contextUriPath);
+        }
+        console.log(tutorialCodeDocument.fileName)
+        //const currentTextFile = editor.document
+        //console.log(currentTextFile.lineAt(0))
+        
+        //editor.edit(editBuilder => {
+        //    editBuilder.insert(new vscode.Position(0,0),"#EnglishPlus: "+tutorialCodeDocument?.);
+        //});
+	}));
+
+    context.subscriptions.push(vscode.commands.registerCommand('english-plus.changeSourceFile', async () => {
+        vscode.commands.executeCommand('english-plus.openCodeFromTutorial',true)
 	}));
 }
 

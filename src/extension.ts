f340f890059e804217ed95384bc395b0fde32c9c
Helpful Projects <helpfulprojectsgithub@gmail.com>
2024-06-20 19:05:46 +0300

    cancel file selection does not show error now

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-async function pickFile(label:string): Promise<vscode.TextDocument>{
+async function pickFile(label:string): Promise<vscode.TextDocument|undefined>{
     const pickedFiles = await vscode.window.showOpenDialog({
         filters: {
           "All files (*.*)": ["*"],
@@ -12,7 +12,7 @@ async function pickFile(label:string): Promise<vscode.TextDocument>{
       });
   
     if (!pickedFiles || pickedFiles.length < 1) {
-        throw new Error('Parameter is not a number!');
+        return
     }
     const pickedFile = pickedFiles[0];
     const textDocument = await vscode.workspace.openTextDocument(pickedFile);
@@ -26,7 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
             return
         }
         const contextUriPath = context.globalState.get(editor.document.fileName)
-        let tutorialCodeDocument: vscode.TextDocument;
+        let tutorialCodeDocument: vscode.TextDocument | undefined;
         if(!contextUriPath || ignoreContextUriPath){
             tutorialCodeDocument = await pickFile("Select file with code from tutorial...");
             if(!tutorialCodeDocument){
@@ -36,13 +36,7 @@ export function activate(context: vscode.ExtensionContext) {
         }else{
             tutorialCodeDocument = await vscode.workspace.openTextDocument(contextUriPath);
         }
-        console.log(tutorialCodeDocument.fileName)
-        //const currentTextFile = editor.document
-        //console.log(currentTextFile.lineAt(0))
         
-        //editor.edit(editBuilder => {
-        //    editBuilder.insert(new vscode.Position(0,0),"#EnglishPlus: "+tutorialCodeDocument?.);
-        //});
 	}));
 
     context.subscriptions.push(vscode.commands.registerCommand('english-plus.changeSourceFile', async () => {

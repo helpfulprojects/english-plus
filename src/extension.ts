198b4558d43a2d13fb7d04ae3dfd3bf3f3e6d852
Helpful Projects <helpfulprojectsgithub@gmail.com>
2024-06-21 11:21:27 +0300

    Creates a map of definition and what to replace it with

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
 
+let definitionsMap: Map<string,string>;
+
 async function pickFile(label:string): Promise<vscode.TextDocument|undefined>{
     const pickedFiles = await vscode.window.showOpenDialog({
         filters: {
@@ -36,7 +38,42 @@ export function activate(context: vscode.ExtensionContext) {
         }else{
             tutorialCodeDocument = await vscode.workspace.openTextDocument(contextUriPath);
         }
-        
+        definitionsMap = new Map<string,string>();
+        const commentsStartWith = "#"
+
+        for(let line = 1; line < tutorialCodeDocument.lineCount; line++){
+            const previousLine = tutorialCodeDocument.lineAt(line-1) 
+            const currentLine = tutorialCodeDocument.lineAt(line);
+            //we are looking for two types lines
+            //line with code and comment above it
+            //line with a comment with a comment above it
+            const isPreviousLineComment = previousLine.text.startsWith(commentsStartWith,previousLine.firstNonWhitespaceCharacterIndex)
+            const isCurrentLineComment = currentLine.text.startsWith(commentsStartWith,currentLine.firstNonWhitespaceCharacterIndex)
+            const isCurrentLineCode = !isCurrentLineComment && !currentLine.isEmptyOrWhitespace
+            if(!isPreviousLineComment){
+                continue;
+            }
+            let definition = previousLine.text.substring(previousLine.firstNonWhitespaceCharacterIndex)
+            let replacement = currentLine.text.substring(currentLine.firstNonWhitespaceCharacterIndex)
+            if(isCurrentLineComment){
+                let definitionArguments: string[] = []
+                definition = definition.replace(/\((.*?)\)/g, (argumentIdentifier) => {
+                    definitionArguments.push(argumentIdentifier)
+                    return "()";
+                });
+                definition = definition.toLowerCase()
+                definitionArguments.forEach((argumentIdentifier, index) => {
+                    replacement = replacement.replaceAll(argumentIdentifier, "~(~" + index + "~)~");
+                });
+                line++;
+            }else if(isCurrentLineCode){
+                line++;    
+            }else{
+                continue;
+            }
+            
+            definitionsMap.set(definition, replacement);
+        }
 	}));
 
     context.subscriptions.push(vscode.commands.registerCommand('english-plus.changeSourceFile', async () => {
